ff292f79fbd05e574cf7cf9f508148dbea02cf80
Josh <joshh2201@gmail.com>
2023-06-11 18:14:06 -0400

    Refactor course and parts into one object

--- part1/src/App.js
+++ part1/src/App.js
@@ -16,36 +16,39 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
+  const [part1, part2, part3] = props.parts;
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
     </div>
   );
 };
 
 const Total = (props) => {
+  const total = props.parts.reduce((sum, curr) => sum + curr.exercises, 0);
+  console.log(total);
   return (
     <>
-    <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {total}</p>
     </>
-  )
-}
+  );
+};
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 },
+    ],
+  };
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
