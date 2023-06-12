2877f509eaff259e2bcf1dccad899e02a119c6e1
Josh <joshh2201@gmail.com>
2023-06-12 19:16:36 -0400

    Add starter code to App.js

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,56 +1,24 @@
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  );
-};
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.name} {props.exercises}
-      </p>
-    </>
-  );
-};
+import { useState } from 'react'
 
-const Content = (props) => {
-  const [part1, part2, part3] = props.parts;
-  return (
-    <div>
-      <Part name={part1.name} exercises={part1.exercises} />
-      <Part name={part2.name} exercises={part2.exercises} />
-      <Part name={part3.name} exercises={part3.exercises} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  const total = props.parts.reduce((sum, curr) => sum + curr.exercises, 0);
-  console.log(total);
-  return (
-    <>
-      <p>Number of exercises {total}</p>
-    </>
-  );
-};
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      { name: 'Fundamentals of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 },
-    ],
-  };
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      {anecdotes[selected]}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
