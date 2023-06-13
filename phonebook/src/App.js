d9fd855738d4a2b5fc534b114d5fdcd7e9fb6726
Josh <joshh2201@gmail.com>
2023-06-13 08:38:20 -0400

    Add eventHandlers for adding new names

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,26 +1,41 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addNewName = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    console.log(newPerson);
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  };
+
+  const personCopy = [...persons];
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {personCopy.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
