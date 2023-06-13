79326d210b23177383f250956f38fab87f9a802b
Josh <joshh2201@gmail.com>
2023-06-12 22:23:35 -0400

    Refactor Content to take a dynamic number of parts

--- part1/course-info/src/App.js
+++ part1/course-info/src/App.js
@@ -16,41 +16,49 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
-  const [part1, part2, part3] = props.parts;
+  const parts = [...props.parts];
   return (
     <div>
-      <Part name={part1.name} exercises={part1.exercises} />
-      <Part name={part2.name} exercises={part2.exercises} />
-      <Part name={part3.name} exercises={part3.exercises} />
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
     </div>
   );
 };
 
 const Total = (props) => {
   const total = props.parts.reduce((sum, curr) => sum + curr.exercises, 0);
-  console.log(total);
   return (
     <>
-      <p>Number of exercises {total}</p>
+      <p>
+        <b>Number of exercises {total}</b>
+      </p>
     </>
   );
 };
+
+const Course = (props) => {
+  const { course } = props;
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
-      { name: 'Fundamentals of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 },
+      { name: 'Fundamentals of React', exercises: 10, id: 1 },
+      { name: 'Using props to pass data', exercises: 7, id: 2 },
+      { name: 'State of a component', exercises: 14, id: 3 },
+      { name: 'Redux', exercises: 8, id: 4 },
     ],
   };
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
