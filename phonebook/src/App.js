d7bb39d77e2553642746bb945f9ddd0dcee81ce8
Josh <joshh2201@gmail.com>
2023-06-13 17:10:36 -0400

    Add search bar to filter names

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [query, setQuery] = useState('');
 
   const handleNameChange = (event) => {
     // update newName as users type in input
@@ -17,10 +21,14 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleQueryChange = (event) => {
+    // update newName as users type in input
+    setQuery(event.target.value);
+  };
   const addNewName = (event) => {
     event.preventDefault();
     // filter to check if newName is in persons already
-    const nameExists = personCopy.filter((person) => person.name === newName);
+    const nameExists = persons.filter((person) => person.name === newName);
     if (nameExists.length > 0) {
       alert(`${newName} is already added to phonebook`);
     } else {
@@ -31,11 +39,21 @@ const App = () => {
     }
   };
 
-  const personCopy = [...persons];
+  const personsFiltered = persons.filter((person) => {
+    if (query !== '') {
+      const name = person.name.toLowerCase();
+      return name.search(query.toLowerCase()) !== -1;
+    }
+    return true;
+  });
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={query} onChange={handleQueryChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -48,7 +66,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {personCopy.map((person) => (
+      {personsFiltered.map((person) => (
         <p key={person.name}>
           {person.name} {person.number}
         </p>
