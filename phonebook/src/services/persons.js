004c56c93871373d32ce0ce855ac04d754f52195
Josh <joshh2201@gmail.com>
2023-06-17 16:54:02 -0400

    Add method to delete a person from the server

--- phonebook/src/services/persons.js
+++ phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = 'http://localhost:3001/persons/';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
@@ -9,9 +9,15 @@ const getAll = () => {
 const create = (newPerson) => {
   const request = axios.post(baseUrl, newPerson);
   return request.then((response) => {
-    console.log(response.data);
     return response.data;
   });
 };
 
-export default { getAll, create };
+const remove = (id) => {
+  const request = axios.delete(`${baseUrl + id}`);
+  return request.then((response) => {
+    return response.data;
+  });
+};
+
+export default { getAll, create, remove };
