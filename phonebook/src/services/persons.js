3af9d877ae4017ba7f9eaccfe08fdbec5fd0cff6
Josh <joshh2201@gmail.com>
2023-06-20 19:03:30 -0400

    Change baseUrl to use routes in phonebook_backend

--- phonebook/src/services/persons.js
+++ phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = 'http://localhost:3001/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
@@ -21,7 +21,7 @@ const remove = (id) => {
 };
 
 const update = (id, updatedPerson) => {
-    const request = axios.put(`${baseUrl}/${id}`,updatedPerson)
-    return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, updatedPerson);
+  return request.then((response) => response.data);
+};
 export default { getAll, create, remove, update };
