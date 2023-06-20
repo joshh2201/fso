64cae0708e05b292bf0e11b0f428cd8c82d98c3a
Josh <joshh2201@gmail.com>
2023-06-20 19:34:47 -0400

    Check env variables for PORT number

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -89,5 +89,5 @@ app.delete('/api/persons/:id', (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
