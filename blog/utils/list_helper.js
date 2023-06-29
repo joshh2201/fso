0de897376644cebbcb3b9c785765c22ef12a9361
Josh <joshh2201@gmail.com>
2023-06-29 16:18:55 -0400

    Create function to calculate total likes

--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (total, blog) => (total += blog.likes);
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
