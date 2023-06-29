04f68ff703cf61bc61f4e395ac289fc5ef123fdf
Josh <joshh2201@gmail.com>
2023-06-29 15:07:05 -0400

    Create get route to see all blog posts

--- blog/index.js
+++ blog/index.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
