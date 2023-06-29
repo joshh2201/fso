92ec02f30af3af5aa1a1a5e4661b2efcf7c573bb
Josh <joshh2201@gmail.com>
2023-06-29 16:47:55 -0400

    Create function to return most liked blog

--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -6,8 +6,17 @@ const totalLikes = (blogs) => {
   const reducer = (total, blog) => (total += blog.likes);
   return blogs.reduce(reducer, 0);
 };
-
+const favoriteBlog = (blogs) => {
+  const reducer = (currFav, blog) => {
+    if (!currFav || currFav.likes < blog.likes) {
+      currFav = blog;
+    }
+    return currFav;
+  };
+  return blogs.reduce(reducer, null);
+};
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
