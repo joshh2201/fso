99219a7711be0d813dffc0573f204557cd3aed5b
Josh <joshh2201@gmail.com>
2023-07-21 13:38:42 -0400

    Add bmi endpoint

--- patientor/index.ts
+++ patientor/index.ts
@@ -1,8 +1,26 @@
-const express = require('express');
+import express, { Request, Response, NextFunction } from 'express';
+import { calculateBmi } from './utils/bmiCalculator';
+import { isNotNumber } from './utils/utils';
+
 const app = express();
 
-app.get('/hello', (_req: any, res: { send: (arg0: string) => void; }) => {
-  res.send('Hello Full Stack');
+function bmiParamValidator(req: Request, res: Response, next: NextFunction) {
+  const { height, weight } = req.query;
+  if (!height || !weight) {
+    return res.status(400).json({ error: 'Missing query parameters' });
+  }
+  const parsedHeight = Number(height);
+  const parsedWeight = Number(weight);
+  if (isNotNumber(parsedHeight) || isNotNumber(parsedWeight)) {
+    return res.status(400).json({ error: 'Height and weight must be numbers' });
+  }
+  next();
+}
+
+app.get('/bmi', bmiParamValidator, (req: Request, res: Response) => {
+  const height = Number(req.query.height)
+  const weight = Number(req.query.weight)
+  res.json({ height, weight, bmi: calculateBmi(height, weight) });
 });
 
 const PORT = 3003;
