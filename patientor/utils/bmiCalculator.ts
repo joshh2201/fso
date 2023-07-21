ef9b604e57ec5e99ff38b82bc4b1e3e14f09e826
Josh <joshh2201@gmail.com>
2023-07-21 13:38:24 -0400

    Remove argument checker

--- patientor/utils/bmiCalculator.ts
+++ patientor/utils/bmiCalculator.ts
@@ -1,24 +1,9 @@
-import { isNotNumber } from './utils';
-interface bmiValues {
+export interface bmiValues {
   height: number;
   weight: number;
 }
 
-const parseArguments = (args: string[]): bmiValues => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
-
-  if (!isNotNumber(args[2]) && !isNotNumber(args[3])) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
-    };
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
-};
-
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   // height in cm, weight in kg
   if (height === 0) throw new Error('Can\t divide by 0!');
   const bmi: number = weight / (height / 100) ** 2; // convert height to m
@@ -32,14 +17,3 @@ const calculateBmi = (height: number, weight: number): string => {
     return 'Obese';
   }
 };
-
-try {
-  const { height, weight } = parseArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.';
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
-}
